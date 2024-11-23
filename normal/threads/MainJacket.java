28309ad067b192ceed59bf5fd206ed8b0a4a6be9
Pedro Santos <pedro_henriquems321@hotmail.com>
2024-11-23 22:23:48 +0000

    Simple threads.

---
+++ normal/threads/MainJacket.java
@@ -0,0 +1,25 @@
+package normal.threads;
+
+public class MainJacket {
+
+    private static void handleUserRequest() {
+        System.out.println("Starting  Thread" + Thread.currentThread());
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("End  Thread" + Thread.currentThread());
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Starting Main");
+
+        for (int i = 0; i < 10000; i++) {
+            new Thread(MainJacket::handleUserRequest).start();
+        }
+
+        System.out.println("End Main");
+    }
+}
